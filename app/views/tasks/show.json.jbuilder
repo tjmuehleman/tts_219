30ba19c8f72986803f3efbdec25b8e11e753d3bf
TJ Muehleman <tj.muehleman@gmail.com>
2014-02-19 08:39:15 -0500

    loading up the codebase

---
+++ app/views/tasks/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @task, :id, :user_id, :name, :due_date, :created_at, :updated_at
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Tts219::Application.config.session_store :cookie_store, key: '_tts_219_session'
